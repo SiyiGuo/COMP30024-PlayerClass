bfec19d412c6811de4c786cf2ce4a8e1983b6d41
Unknown <1578942650@qq.com>
2018-05-09 12:21:23 +1000

    adding depth as class attribute

--- ABsearch.py
+++ ABsearch.py
@@ -14,7 +14,8 @@ class Absearch():
     def __init__(self, game, player):
         #Player will always be White(1/friend), as we pass in canonical board
         self.game = game
-        self.player = player        
+        self.player = player
+        self.abpDepth = 4
     
     def timeOut(self):
         if abs(time.time() - self.time) > 20:
